51c56ae3018cca4d6eea45fed353c22f44be195c
KEIKO SHIMIZU <106935804+KeikoShimizu@users.noreply.github.com>
2023-06-13 13:58:24 -0700

    completed

--- app/page.js
+++ app/page.js
@@ -1,25 +1,59 @@
 "use client"
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Updock } from 'next/font/google';
 
 export default function Home() {
+  // const [ myElementIsVisible, setMyElementIsVisible ] = useState();
+  
+  // const myRef = useRef()
 
-  const myRef = useRef()
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries, observer) => {
+  //     const entry = entries[0];
 
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      const entry = entries[0];
-      console.log('entry', entry);
-      console.log('entry.isTntersecting', entry.isIntersecting);
-    });
-    observer.observe(myRef.current);
-    console.log('myRef', myRef.current)
-  },[]);
+  //     setMyElementIsVisible(entry.isIntersecting);
+
+  //     //If the element is focusd = TRUE / not Focused = false
+  //     console.log('entry', entry);
+  //     console.log('entry.isTntersecting', entry.isIntersecting);
+  //   });
+
+  //   observer.observe(myRef.current);
+
+  //   //Observing されるElement
+  //   console.log('myRef', myRef.current)
+  // },[]);
+
+  //react-intersection-observer を使うと！これだけでObserveすることができる！
+  //Ref = 発火Point　　　　inView= useState hookのようなもの（true | false）
+  const { ref: myRef, inView: myElementIsVisible } = useInView();
+  const { ref: magicSectionRef, inView: magicSectionIsVisible } = useInView();
+
 
   return (
-    <main className={styles.main}>
-     <h1 ref={myRef} className={styles.heading}>He Hey Hey</h1>
-    </main>
+
+    <>
+    <header className={styles.header}>
+    </header>
+      <main className={styles.main}>
+       <div className={styles.section1}>
+         <h1 ref={myRef} className={styles.heading}>He Hey Hey</h1>
+         <p className={styles.p}>{myElementIsVisible ? 'YES FOCUSED' : 'NO'}</p>
+       </div>
+
+       <div className={styles.section2}>
+         <h2 ref={magicSectionRef} className={styles.h2}>Here is Place to fly rocket!</h2>
+         <span className={`${styles.rocket} ${magicSectionIsVisible ? styles.animateRocket : ''}`}>
+          🚀
+         </span>
+       </div>
+      </main>
+      <footer className={styles.footer}>
+
+      </footer>
+    </>
   )
 }
